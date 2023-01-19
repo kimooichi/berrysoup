ac40ae418bd7b4c20e18a2794d8a20b3d6972365
kimooichi <118089544+kimooichi@users.noreply.github.com>
2023-01-19 16:11:50 -0500

    Add files via upload

---
+++ BerrySoupFAQ.js
@@ -0,0 +1,83 @@
+$(document).ready( /* Answer is revealed using click and slidetoggle */
+	function ()
+	{
+		$("#Answer1").hide();
+
+		$("#Question1").click(slide1);
+	}
+);
+
+function slide1 ()
+{
+	$("#Answer1").slideToggle();
+}
+
+$(document).ready( /* Answer is revealed using click and slidetoggle */
+	function ()
+	{
+		$("#Answer2").hide();
+
+		$("#Question2").click(slide2);
+	}
+);
+
+function slide2 ()
+{
+	$("#Answer2").slideToggle();
+}
+
+$(document).ready( /* Answer is revealed using click and slidetoggle */
+	function ()
+	{
+		$("#Answer3").hide();
+
+		$("#Question3").click(slide3);
+	}
+);
+
+function slide3 ()
+{
+	$("#Answer3").slideToggle();
+}
+
+$(document).ready( /* Answer is revealed using click and slidetoggle */
+	function ()
+	{
+		$("#Answer4").hide();
+
+		$("#Question4").click(slide4);
+	}
+);
+
+function slide4 ()
+{
+	$("#Answer4").slideToggle();
+}
+
+$(document).ready( /* Answer is revealed using click and slidetoggle */
+	function ()
+	{
+		$("#Answer5").hide();
+
+		$("#Question5").click(slide5);
+	}
+);
+
+function slide5 ()
+{
+	$("#Answer5").slideToggle();
+}
+
+$(document).ready( /* Answer is revealed using click and slidetoggle */
+	function ()
+	{
+		$("#Answer6").hide();
+
+		$("#Question6").click(slide6);
+	}
+);
+
+function slide6 ()
+{
+	$("#Answer6").slideToggle();
+}
